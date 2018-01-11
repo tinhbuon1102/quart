d5eff0a80efbc7109cbdcace0ab36051df17bb7e
tinhbuon1102 <quocthang.2001@gmail.com>
2018-01-11 20:09:19 +0900

    Upload files

--- wp-content/themes/mystile/functions.php
+++ wp-content/themes/mystile/functions.php
@@ -6,7 +6,6 @@ if ( ! empty( $_SERVER['SCRIPT_FILENAME'] ) && basename( __FILE__ ) == basename(
 ?>
 <?php
 
-
 /*-----------------------------------------------------------------------------------*/
 /* Start WooThemes Functions - Please refrain from editing this section */
 /*-----------------------------------------------------------------------------------*/
@@ -46,7 +45,14 @@ foreach ( $includes as $i ) {
 /*-----------------------------------------------------------------------------------*/
 
 
+add_filter( 'loop_shop_per_page', 'new_loop_shop_per_page', 20 );
 
+function new_loop_shop_per_page( $cols ) {
+  // $cols contains the current number of products per page based on the value stored on Options -> Reading
+  // Return the number of products you wanna show per page.
+  $cols = 24;
+  return $cols;
+}
 
 
 //管理バーを非表示
